907ae7af13bb1515a8a55e70a939551b8ac823a7
josandg <gussanjoba@student.gu.se>
2023-05-13 16:27:43 +0200

    inputbox transition

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -4,6 +4,7 @@ import { ProgressBar } from "~/components/ProgressBar";
 import { useForm } from "react-hook-form";
 import { useRouter } from "next/router";
 import json from "../../house.json";
+import { Transition } from "@headlessui/react";
 
 type FormValues = {
   guess: number;
@@ -100,46 +101,89 @@ export default function game() {
         </span>
         <img className="w-full" src={gameData.house.images[0]} alt="" />
 
-        <form
-          onSubmit={handleGuess}
-          className="rounded-xl rounded-t bg-gray-200"
+        <Transition
+          is="fragment"
+          show={true}
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
         >
-          <span className="flex flex-col p-4">
-            <div className="relative flex items-center">
-              <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-gray-500">
-                $
-              </span>
-              <input
-                {...register("guess", { required: true })}
-                placeholder="enter the price"
-                className="w-full rounded-lg border-2 border-gray-300 py-2 pl-10 pr-3 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:ring-opacity-50"
-              />
-            </div>
+          {currentGuessIndex === gameData.guesses.length ? (
+            <form
+              key="guess-form"
+              onSubmit={handleGuess}
+              className="rounded-xl rounded-t bg-gray-200"
+            >
+              <span className="flex flex-col p-4">
+                <div className="relative flex items-center">
+                  <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-gray-500">
+                    $
+                  </span>
+                  <input
+                    {...register("guess", { required: true })}
+                    placeholder="enter the price"
+                    className="w-full rounded-lg border-2 border-gray-300 py-2 pl-10 pr-3 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:ring-opacity-50"
+                  />
+                </div>
 
-            <div className="flex w-full items-center justify-center">
-              <button
-                type="button"
-                onClick={handleGoBackToLastGuess}
-                className="flex-1 border bg-gray-500 p-2"
-              >
-                LEFT{" "}
-              </button>
-              <button
-                type="submit"
-                className="flex-[3] bg-green-700 p-2 text-2xl text-white"
-              >
-                Guess
-              </button>
-              <button
-                type="button"
-                onClick={handleGoToNextGuess}
-                className="flex-1 border bg-gray-500 p-2"
-              >
-                RIGHT
-              </button>
-            </div>
-          </span>
-        </form>
+                <div className="flex w-full items-center justify-center">
+                  <button
+                    type="button"
+                    onClick={handleGoBackToLastGuess}
+                    className="flex-1 border bg-gray-500 p-2"
+                  >
+                    LEFT{" "}
+                  </button>
+                  <button
+                    type="submit"
+                    className="flex-[3] bg-green-700 p-2 text-2xl text-white"
+                  >
+                    Guess
+                  </button>
+                  <button
+                    type="button"
+                    onClick={handleGoToNextGuess}
+                    className="flex-1 border bg-gray-500 p-2"
+                  >
+                    RIGHT
+                  </button>
+                </div>
+              </span>
+            </form>
+          ) : (
+            <form
+              key="current-guess-form"
+              onSubmit={() => console.log("submit")}
+              className="rounded-xl rounded-t bg-gray-200"
+            >
+              <span className="flex flex-col p-4">
+                <div className="flex w-full items-center justify-center">
+                  <button
+                    type="button"
+                    onClick={handleGoBackToLastGuess}
+                    className="flex-1 border bg-gray-500 p-2"
+                  >
+                    LEFT{" "}
+                  </button>
+                  <button
+                    type="submit"
+                    className="flex-[3] bg-green-700 p-2 text-2xl text-white"
+                  >
+                    Go back to current
+                  </button>
+                  <button
+                    type="button"
+                    onClick={handleGoToNextGuess}
+                    className="flex-1 border bg-gray-500 p-2"
+                  >
+                    RIGHT
+                  </button>
+                </div>
+              </span>
+            </form>
+          )}
+        </Transition>
       </div>
     </div>
   );
