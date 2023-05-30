840457a7f7a4edee8e9acf18a16b45fb1cc54a71
josandg <gussanjoba@student.gu.se>
2023-05-30 23:24:57 +0200

    cpoint

--- core/api.py
+++ core/api.py
@@ -5,11 +5,15 @@ import threading
 import time
 import schedule
 
+
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 from hemnetListingScraper import scrape_hemnet_listing
 
 app = Flask(__name__)
+# make it so everyone has access to the api
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 
 data = {}
@@ -19,7 +23,10 @@ urls = [
     "https://www.hemnet.se/bostad/lagenhet-3rum-sjobo-boras-kommun-barnhemsgatan-42-20057795",
     "https://www.hemnet.se/bostad/lagenhet-2rum-centrum-boras-kommun-arlagatan-4-20057596",
     "https://www.hemnet.se/bostad/lagenhet-2rum-tullen-boras-kommun-fjallgatan-50b-20055978",
-    "https://www.hemnet.se/bostad/radhus-5rum-boras-boras-kommun-hogloftsgatan-5-20055235"
+    "https://www.hemnet.se/bostad/radhus-5rum-boras-boras-kommun-hogloftsgatan-5-20055235",
+    "https://www.hemnet.se/bostad/gard-knared-laholms-kommun-jons-petters-gard,-tralshult-55-20059896",
+    "https://www.hemnet.se/bostad/lagenhet-1rum-torpa-jonkopings-kommun-erik-dahlbergsgatan-4-16835522"
+    "https://www.hemnet.se/bostad/fritidsboende-2rum-bjorsjo-avesta-kommun-bjorsjo-11-20059915"
 ]
 
 
@@ -30,7 +37,7 @@ def scrape_data():
 
 
 def scrape_job():
-    schedule.every(60).seconds.do(scrape_data)
+    schedule.every(25).seconds.do(scrape_data)
 
     while True:
         schedule.run_pending()
@@ -43,7 +50,7 @@ def retreieveJsonData():
         return data
 
 
-@app.route('/getRandomListing', methods=['GET'])
+@app.route('/api/getRandomListing', methods=['GET'])
 def getRandomListing():
     # get the url from the request and scrape the data
     # import json file from data.json
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { ToastContainer } from "react-toastify";
 
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={new QueryClient()}>
+      <Component {...pageProps} />;
+    </QueryClientProvider>
+  );
 };
 
 export default MyApp;
